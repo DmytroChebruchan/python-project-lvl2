e749032028bc4ff91830e3cb881f846e3c8aed3a
Dmitriy <chebruchan2707@gmail.com>
2022-04-08 23:42:50 +0300

    autocommit

--- Makefile
+++ Makefile
@@ -1,9 +1,26 @@
 build:
+	export PATH=$PATH:$HOME/.poetry/bin
 	poetry build
 
 publish:
-	poetry publish --dry-run
+	poetry publish --dry-run -u ' ' -p ' '
+
+install:
+	python3 -m pip install --user dist/*.whl
+	export PATH=$PATH:$HOME/Library/Python/3.10/bin
 
 package-install:
 	python3 -m pip install --force-reinstall --user dist/*.whl
-	export PATH=$PATH:$HOME/Library/Python/3.10/bin
+
+update:
+	make build
+	make publish
+	make package-install
+
+test:
+	@poetry run python -m gendiff.scripts.gendiff -f 'JSON' file1.json file2.json
+
+push:
+	git add .
+	git commit -m 'autocommit'
+	git push
--- gendiff/scripts/gendiff.py
+++ gendiff/scripts/gendiff.py
@@ -1,17 +1,78 @@
 #!/usr/bin/env python3
 
-import argparse
+import argparse, json
 
 parser = argparse.ArgumentParser(description='Generate diff')
 parser.add_argument('first_file')
 parser.add_argument('second_file')
 parser.add_argument('-f', '--format', 
         help = 'set format of output')
-args = parser.parse_args()
+args = parser.parse_args() 
 
+
+def generate_diff():
+
+    first_dict = dict(json.load(open('file1.json')))
+    second_dict = dict(json.load(open('file2.json')))
+
+    merged_unique_keys_list = sorted(first_dict | second_dict)
+    merged_dict ={}
+    merged_list =[]
+    result = '{ \n'
+
+    # replacing False and True to false and true as string
+    for key in first_dict:
+        if first_dict[key] is False:
+            first_dict[key] = 'false'
+        elif first_dict[key] is True:
+            first_dict[key] = 'true' 
+
+    for key in second_dict:
+        if second_dict[key] is False:
+            second_dict[key] = 'false'
+        elif second_dict[key] is True:
+            second_dict[key] = 'true' 
+
+    #creating merged dict
+    for key in merged_unique_keys_list:
+        if key in first_dict and key in second_dict:
+            if first_dict[key] == second_dict[key]:
+                merged_dict[key] = [first_dict[key]]
+            else:
+                merged_dict[key] = [first_dict[key], second_dict[key]] 
+        elif key in first_dict:
+            merged_dict[key] = [first_dict[key], None]
+        else:
+            merged_dict[key] = [None, second_dict[key]]
+
+    # creating merged incripted list with all information
+    for key in merged_dict:
+        merged_list.append([key, merged_dict[key]])
+    
+    merged_list.sort()
+
+    # generating string from incripted list
+    for element in merged_list:
+        if len(element[1]) == 1:
+            result = result + '    "' + str(element[0]) + '": ' + str(element[1][0]) +'\n'
+        elif element[1][0] is None:
+            result = result + '  + "' + str(element[0]) + '": ' + str(element[1][1]) +'\n'
+        elif element[1][1] is None:
+            result = result + '  - "' + str(element[0]) + '": ' + str(element[1][0]) +'\n'
+        else:
+            result = result + '  - "' + str(element[0]) + '": ' + str(element[1][0]) +'\n'
+            result = result + '  + "' + str(element[0]) + '": ' + str(element[1][1]) +'\n' 
+
+
+    result = result + "}"
+    print(result)
+
+    return result
+
+    
 
 def main():
-    print(args.echo)
+    generate_diff()
 
 
 if __name__ == '__main__':
