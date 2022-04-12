3bade249cf891d261fb51d14cdedf99bba584395
Dmitriy <chebruchan2707@gmail.com>
2022-04-12 11:03:15 +0300

    autocommit

--- gendiff/scripts/gendiff.py
+++ gendiff/scripts/gendiff.py
@@ -13,30 +13,22 @@ parser.add_argument('-f', '--format',
 args = parser.parse_args()
 
 
-def generate_diff(first_file=args.first_file, second_file=args.second_file):
+# changes boolin type to string with small first letter
+def replace_F_T_to_f_t(first_dict):
 
-    first_dict = dict(json.load(open(first_file)))
-    second_dict = dict(json.load(open(second_file)))
-
-    merged_unique_keys_list = sorted(first_dict | second_dict)
-    merged_dict = {}
-    merged_list = []
-    result = '{ \n'
-
-    # replacing False and True to false and true as string
     for key in first_dict:
         if first_dict[key] is False:
             first_dict[key] = 'false'
         elif first_dict[key] is True:
             first_dict[key] = 'true'
 
-    for key in second_dict:
-        if second_dict[key] is False:
-            second_dict[key] = 'false'
-        elif second_dict[key] is True:
-            second_dict[key] = 'true'
+    return first_dict
 
-    # creating merged dict
+
+# merger of dicts saving source of keys and their values
+def dict_merger(merged_unique_keys_list, first_dict, second_dict):
+    merged_dict = {}
+
     for key in merged_unique_keys_list:
         if key in first_dict and key in second_dict:
             if first_dict[key] == second_dict[key]:
@@ -48,13 +40,24 @@ def generate_diff(first_file=args.first_file, second_file=args.second_file):
         else:
             merged_dict[key] = [None, second_dict[key]]
 
-    # creating merged incripted list with all information
-    for key in merged_dict:
-        merged_list.append([key, merged_dict[key]])
+    return merged_dict
 
-    merged_list.sort()
 
-    # generating string from incripted list
+# changer of dict to sorted list for further decoding
+def dict_to_sorted_list(dictionary):
+    result = []
+
+    for key in dictionary:
+        result.append([key, dictionary[key]])
+    result.sort()
+
+    return result
+
+
+# decoder of complex list to required string format
+def result_generator(merged_list):
+    result = '{ \n'
+
     for element in merged_list:
         if len(element[1]) == 1:
             result = result + '    "'\
@@ -72,6 +75,31 @@ def generate_diff(first_file=args.first_file, second_file=args.second_file):
                 + str(element[0]) + '": ' + str(element[1][1]) + '\n'
 
     result = result + "}"
+    return result
+
+
+def generate_diff(first_file=args.first_file, second_file=args.second_file):
+
+    first_dict = dict(json.load(open(first_file)))
+    second_dict = dict(json.load(open(second_file)))
+
+    merged_unique_keys_list = sorted(first_dict | second_dict)
+    merged_dict = {}
+    merged_list = []
+    result = '{ \n'
+
+    # replacing False and True to false and true as string
+    first_dict = replace_F_T_to_f_t(first_dict)
+    second_dict = replace_F_T_to_f_t(second_dict)
+
+    # creating merged dict
+    merged_dict = dict_merger(merged_unique_keys_list, first_dict, second_dict)
+
+    # creating merged incripted list with all information
+    merged_list = dict_to_sorted_list(merged_dict)
+
+    # decoded result
+    result = result_generator(merged_list)
 
     return result
 
--- tests/test_gendiff.py
+++ tests/test_gendiff.py
@@ -1,6 +1,11 @@
 from gendiff.scripts.gendiff import generate_diff
 
-file = open('./tests/fixtures/fixture_gendiff_test.txt', 'r')
-result = file.read()
 
-assert generate_diff('file1.json', 'file2.json') == str(result)
+def generate_diff_test():
+    file = open('./tests/fixtures/fixture_gendiff_test.txt', 'r')
+    result = file.read()
+
+    assert generate_diff('file1.json', 'file2.json') == str(result)
+
+
+generate_diff_test()
