2bd8c7e4683340fc2f887ac8157f131d186e311a
Dmitriy <chebruchan2707@gmail.com>
2022-04-16 21:02:50 +0300

    changed name of test fundtionto test_

--- tests/test_gendiff.py
+++ tests/test_gendiff.py
@@ -1,9 +1,9 @@
 from gendiff.scripts.gendiff import generate_diff
 
 
-def generate_diff_test():
+def test_generate_diff():
     file = open('./tests/fixtures/fixture_gendiff_test.txt', 'r')
     result = file.read()
     assert generate_diff('file1.json', 'file2.json') == str(result)
 
-generate_diff_test()
+test_generate_diff()
