b992ff375cee3805d010b9b580ee6bb954ed411f
Dmitriy <chebruchan2707@gmail.com>
2022-04-17 18:30:54 +0300

    test with YAML added

--- gendiff/scripts/gendiff.py
+++ gendiff/scripts/gendiff.py
@@ -2,6 +2,7 @@
 
 import argparse
 import json
+from parcer import parcer
 
 
 # parcert of positional arguments
--- tests/test_gendiff.py
+++ tests/test_gendiff.py
@@ -1,9 +1,17 @@
 from gendiff.scripts.gendiff import generate_diff
 
 
-def test_generate_diff():
+def test_generate_diff_json():
     file = open('./tests/fixtures/fixture_gendiff_test.txt', 'r')
     result = file.read()
     assert generate_diff('file1.json', 'file2.json') == str(result)
 
-test_generate_diff()
+test_generate_diff_json()
+
+
+def test_generate_diff_yaml():
+    file = open('./tests/fixtures/fixture_gendiff_test.txt', 'r')
+    result = file.read()
+    assert generate_diff('file1.yaml', 'file2.yaml') == str(result)
+
+test_generate_diff_yaml()
