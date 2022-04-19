dce4a0c2d1fef42e94359a6a227f0024481f7061
Dmitriy <chebruchan2707@gmail.com>
2022-04-19 17:21:57 +0300

    updated structure of tested files, multilevel test files added

--- tests/test_gendiff_json.py
+++ tests/test_gendiff_json.py
@@ -2,14 +2,9 @@ from gendiff.scripts.gendiff import generate_diff
 
 
 def test_generate_diff_json():
-    file = open('./tests/fixtures/fixture_gendiff_test.txt', 'r')
+    file = open('./tests/fixtures/results/fixture_gendiff_test.txt', 'r')
     result = file.read()
-    assert generate_diff('tests/fixtures/file1.json', 'tests/fixtures/file2.json') == str(result)
-
-def test_generate_diff_json_with_format():
-    file = open('tests/fixtures/fixture_gendiff_test.txt', 'r')
-    result = file.read()
-    assert generate_diff('tests/fixtures/file1.json', 'tests/fixtures/file2.json', 'JSON') == str(result)
+    assert generate_diff('tests/fixtures/JSON/file1.json', 'tests/fixtures/JSON/file2.json') == str(result)
+    assert generate_diff('tests/fixtures/JSON/file1.json', 'tests/fixtures/JSON/file2.json', 'JSON') == str(result)
 
 test_generate_diff_json()
-test_generate_diff_json_with_format()
--- tests/test_gendiff_yml.py
+++ tests/test_gendiff_yml.py
@@ -2,14 +2,9 @@ from gendiff.scripts.gendiff import generate_diff
 
 
 def test_generate_diff_yaml():
-    file = open('./tests/fixtures/fixture_gendiff_test.txt', 'r')
+    file = open('./tests/fixtures/results/fixture_gendiff_test.txt', 'r')
     result = file.read()
-    assert generate_diff('tests/fixtures/file1.yml', 'tests/fixtures/file2.yml') == str(result)
+    assert generate_diff('tests/fixtures/YML/file1.yml', 'tests/fixtures/YML/file2.yml') == str(result)
+    assert generate_diff('tests/fixtures/YML/file1.yml', 'tests/fixtures/YML/file2.yml', 'yml') == str(result)
 
-def test_generate_diff_yaml_with_format_stated():
-    file = open('./tests/fixtures/fixture_gendiff_test.txt', 'r')
-    result = file.read()
-    assert generate_diff('tests/fixtures/file1.yml', 'tests/fixtures/file2.yml', 'yml') == str(result)
-
-test_generate_diff_yaml_with_format_stated()
 test_generate_diff_yaml()
